4c75b99ef59a46289b0b4c1cbe5cf250ed270952
drishtipala <105123081+drishtipala@users.noreply.github.com>
2023-06-08 20:17:58 +0530

    Delete basic_info.java

--- basic_info.java
+++
@@ -1,5 +0,0 @@
-class basic_info{
-    public static void main(String args[]){
-	    System.out.println("My name is Drishti Pala, Recently Appeared for TY Final examination from Shankar Narayan College");
-	}
-}
